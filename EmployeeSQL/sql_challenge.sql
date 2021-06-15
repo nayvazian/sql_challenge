8d8642ab183f571d8cd6fe3377b022cf1aa9e739
nayvazian <nicholas.ayvazian@gmail.com>
2021-06-15 02:16:33 -0400

    adding new files

--- EmployeeSQL/sql_challenge.sql
+++ EmployeeSQL/sql_challenge.sql
@@ -1,3 +1,18 @@
+DROP TABLE IF EXISTS "departments" CASCADE;
+DROP TABLE IF EXISTS "dept_emp" CASCADE;
+DROP TABLE IF EXISTS "dept_manager" CASCADE;
+DROP TABLE IF EXISTS "employees" CASCADE;
+DROP TABLE IF EXISTS "salaries" CASCADE;
+DROP TABLE IF EXISTS "titles" CASCADE;
+
+CREATE TABLE "titles" (
+    "title_id" varchar(200)   NOT NULL,
+    "title" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
 CREATE TABLE "departments" (
     "dept_no" varchar(200)   NOT NULL,
     "dept_name" varchar(200)   NOT NULL,
@@ -6,19 +21,9 @@ CREATE TABLE "departments" (
      )
 );
 
-CREATE TABLE "dept_emp" (
-    "emp_no" int   NOT NULL,
-    "dept_no" int   NOT NULL
-);
-
-CREATE TABLE "dept_manager" (
-    "emp_no" int   NOT NULL,
-    "dept_no" int   NOT NULL
-);
-
 CREATE TABLE "employees" (
     "emp_no" int   NOT NULL,
-    "emp_title_id" int   NOT NULL,
+    "emp_title_id" varchar(200)   REFERENCES "titles" ("title_id"),
     "birth_date" DATE   NOT NULL,
     "first_name" varchar(200)   NOT NULL,
     "last_name" varchar(200)   NOT NULL,
@@ -29,36 +34,18 @@ CREATE TABLE "employees" (
      )
 );
 
+CREATE TABLE "dept_emp" (
+    "emp_no" int	REFERENCES employees (emp_no),
+    "dept_no" varchar(200)   REFERENCES departments (dept_no)
+);
+
+CREATE TABLE "dept_manager" (
+    "emp_no" int   REFERENCES "employees" ("emp_no"),
+    "dept_no" varchar(200)  REFERENCES "departments" ("dept_no")
+);
+
 CREATE TABLE "salaries" (
-    "emp_no" int   NOT NULL,
+    "emp_no" int   REFERENCES "employees" ("emp_no"),
     "salary" int   NOT NULL
 );
 
-CREATE TABLE "titles" (
-    "title_id" int   NOT NULL,
-    "title" varchar(200)   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY (
-        "title_id"
-     )
-);
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "departments" ("dept_no");
-
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "titles" ("title_id");
-
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-CREATE INDEX "idx_departments_dept_name"
-ON "departments" ("dept_name");
